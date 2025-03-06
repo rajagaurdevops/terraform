ebea4b3d693c968f322717206213ab270a048a8c
rajagaurdevops <your.rajagaur333@gmail.com>
2025-03-06 17:23:57 +0530

    testing

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     encrypt        = true
   }
 }
