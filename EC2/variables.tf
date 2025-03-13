d463d5f0dfe0ae336df3514c7a578a21ad9aa02e
rajagaurdevops <your.rajagaur333@gmail.com>
2025-03-13 17:28:38 +0530

    Initial Terraform setup

---
+++ EC2/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0c55b159cbfafe1f0"  # Ubuntu 22.04 LTS (Change if needed)
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ssh_key" {
+  default = "~/.ssh/authorized_keys"  # Make sure this key exists
+}
