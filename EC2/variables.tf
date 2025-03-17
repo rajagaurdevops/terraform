43c0f1f1cc11b122d9ef6f5cefb057c8bed64023
rajagaurdevops <your.rajagaur333@gmail.com>
2025-03-17 14:50:16 +0530

    launch ec2 instance

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -12,5 +12,5 @@ variable "instance_type" {
 
 variable "ssh_key" {
   description = "Path to SSH public key"
-  default     = "~/.ssh/deployer_key.pub"
+  default     = "/home/ubuntu/.ssh/deployer_key.pub"
 }
