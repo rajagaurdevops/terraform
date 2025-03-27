2cb7a63c10c7eaf8f03def0c19f7a37fa2c5e621
rajagaurdevops <your.rajagaur333@gmail.com>
2025-03-27 10:24:05 +0530

    update

--- aws-vpc-ec2-nginx/ec2.tf
+++ aws-vpc-ec2-nginx/ec2.tf
@@ -3,16 +3,12 @@ resource "aws_instance" "nginxserver" {
   ami                         = "ami-04b4f1a9cf54c11d0"
   instance_type               = "t3.micro"
   key_name =                       "hp"
-
   # Associate the instance with a public subnet
   subnet_id                   = aws_subnet.public-subnet.id
-
   # Attach the security group for Nginx 
   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
-
   # Enable public IP assignment for external access
   associate_public_ip_address = true
-
 # User data script to install and start Nginx on instance launch
   user_data = <<-EOF
             #!/bin/bash
