59035839f1ed384e34bdcf5eca7cf252ef3546c5
rajagaurdevops <rajagaur333@gmail.com>
2025-09-28 12:46:39 +0530

    Create main.tf

---
+++ aks/main.tf
@@ -0,0 +1,123 @@
+provider "azurerm" {
+  features {}
+}
+
+# ----------------------
+# Resource Group
+# ----------------------
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# ----------------------
+# Virtual Network & Subnet
+# ----------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aks-vnet"
+  address_space       = ["10.0.0.0/8"]
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.240.0.0/16"]
+}
+
+# ----------------------
+# Log Analytics Workspace for Monitoring
+# ----------------------
+resource "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "${var.aks_name}-law"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# ----------------------
+# Azure Container Registry (ACR)
+# ----------------------
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+# ----------------------
+# AKS Cluster with RBAC, Managed Identity, Monitoring
+# ----------------------
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = var.aks_dns_prefix
+
+  kubernetes_version  = var.kubernetes_version
+  role_based_access_control_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name                = "default"
+    node_count          = var.node_count
+    vm_size             = var.vm_size
+    vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+    type                = "VirtualMachineScaleSets"
+    os_disk_size_gb     = 30
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 5
+  }
+
+  # Optional spot node pool
+  dynamic "agent_pool_profile" {
+    for_each = var.enable_spot ? [1] : []
+    content {
+      name                = "spotnp"
+      vm_size             = "Standard_DS2_v2"
+      node_count          = 1
+      vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+      type                = "VirtualMachineScaleSets"
+      os_disk_size_gb     = 30
+      enable_auto_scaling = true
+      min_count           = 1
+      max_count           = 2
+      priority            = "Spot"
+      eviction_policy     = "Delete"
+    }
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+    }
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    outbound_type     = "loadBalancer"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# ----------------------
+# Grant AKS Access to ACR
+# ----------------------
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
