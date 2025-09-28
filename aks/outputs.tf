401e991a25865773f33a7d57f82d250e83781806
rajagaurdevops <rajagaur333@gmail.com>
2025-09-28 12:49:15 +0530

    Create outputs.tf

---
+++ aks/outputs.tf
@@ -0,0 +1,12 @@
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
