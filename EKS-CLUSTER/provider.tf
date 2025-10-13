26e9b92937f1212b99a85eadac4ee88c3238143e
rajagaurdevops <rajagaur333@gmail.com>
2025-10-13 16:19:41 +0530

    Create provider.tf

---
+++ EKS-CLUSTER/provider.tf
@@ -0,0 +1,16 @@
+# Terraform version and required providers
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# AWS Provider configuration
+provider "aws" {
+  region = var.aws_region  # Use region from variables.tf
+}
