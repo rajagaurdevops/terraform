665600124deb8a86cfb15684630d1547a41a89a2
rajagaurdevops <rajagaur333@gmail.com>
2025-10-29 21:30:13 +0530

    Refactor EC2 variables for clarity and consistency

--- infrastructure-live/ec2/variables.tf
+++ infrastructure-live/ec2/variables.tf
@@ -1,29 +1,62 @@
 variable "region" {
-    description = "The value of Region"
-    type = string
-    default = "us-east-1"
-  
-}
-
-variable "instance_type" {
-    description = "Ec2 instance type"
-    default = "t3.micro"
-  
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "ami" {
-    description = "AMI ID for EC2 instance"
-    default = "ami-04b4f1a9cf54c11d0"
-  
+  description = "AMI ID"
+  type        = string
+  default     = "ami-04b4f1a9cf54c11d0"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "key_name" {
-    description = " Already exiting key pair"
-    default = "hp"
-  
+  description = "Key pair name for EC2"
+  type        = string
+  default     = "hp"
 }
 
-variable "vpc_security_group_ids" {
-    description = " value of security group"
-    default = ["sg-0be11f9d07ee34fdd"]
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+  default     = "prod-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "Private subnet CIDR"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default = {
+    Environment = "Production"
+    Project     = "InfraDemo"
+  }
 }
