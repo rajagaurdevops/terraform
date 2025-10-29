a304d7351053e738296015f29339d928abdf854c
rajagaurdevops <rajagaur333@gmail.com>
2025-10-29 21:24:46 +0530

    Update provider.tf

--- infrastructure-live/ec2/provider.tf
+++ infrastructure-live/ec2/provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
+provider "aws" {
+  region = var.region
 }
+
