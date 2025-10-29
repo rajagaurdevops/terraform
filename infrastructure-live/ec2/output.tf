7109709aebfd5cf9e393aa6ffadcea7224077b8b
rajagaurdevops <rajagaur333@gmail.com>
2025-10-29 21:31:09 +0530

    Update output.tf

--- infrastructure-live/ec2/output.tf
+++ infrastructure-live/ec2/output.tf
@@ -1,4 +1,14 @@
-output "aws_instance_public_ip" {
-    value = aws_instance.myserver.public_ip
-  
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "VPC ID from module"
+}
+
+output "public_subnet_id" {
+  value       = module.vpc.public_subnet_id
+  description = "Public subnet ID"
+}
+
+output "ec2_public_ip" {
+  value       = aws_instance.web_server.public_ip
+  description = "EC2 instance public IP"
 }
