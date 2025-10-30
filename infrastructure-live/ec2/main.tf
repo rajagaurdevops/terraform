bee175baa3c76416680d556dac78b03877c17029
rajagaurdevops <rajagaur333@gmail.com>
2025-10-30 16:50:26 +0530

    Update main.tf

--- infrastructure-live/ec2/main.tf
+++ infrastructure-live/ec2/main.tf
@@ -2,7 +2,7 @@
 # Use VPC module from another GitHub repo
 # ----------------------------------------------------
 module "vpc" {
-  source =  "git::https://github.com/rajagaurdevops/terraform-modules.git//vic-module?ref=main"
+  source = "git::https://github.com/rajagaurdevops/terraform-modules.git//vic-module?ref=main"
 
   vpc_name            = var.vpc_name
   vpc_cidr            = var.vpc_cidr
@@ -12,13 +12,16 @@ module "vpc" {
   tags                = var.tags
 }
 
+# Show which AWS account is being used
+data "aws_caller_identity" "current" {}
+
 # ----------------------------------------------------
 # EC2 Instance in Public Subnet
 # ----------------------------------------------------
 resource "aws_instance" "web_server" {
   ami                         = var.ami
   instance_type               = var.instance_type
-  key_name                    = var.key_name
+  # key_name                    = var.key_name
   subnet_id                   = module.vpc.public_subnet_id
   associate_public_ip_address  = true
 
