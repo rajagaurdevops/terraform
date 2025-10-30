7036851fe2d0c4e83182b272c61cabfda3ad4e4b
rajagaurdevops <rajagaur333@gmail.com>
2025-10-30 16:52:42 +0530

    Update output.tf

--- infrastructure-live/ec2/output.tf
+++ infrastructure-live/ec2/output.tf
@@ -12,3 +12,7 @@ output "ec2_public_ip" {
   value       = aws_instance.web_server.public_ip
   description = "EC2 instance public IP"
 }
+
+output "current_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
